20693b9a47fbc33e6e89b6e91cb778dbd53e473b
Sumit <sumits@incubxperts.com>
2022-04-25 16:08:27 +0530

    Added terraform module

--- variables.tf
+++ variables.tf
@@ -1,31 +1,57 @@
-variable "service_account_credentials" {}
+variable "service_account_credentials" {
+  description = "Service account email Id."
+}
 
-variable "project_name" {}
+variable "project_name" {
+  description = "studied-client-346013"
+}
 
-variable "region" {}
+variable "region" {
+  description = "Service region"
+}
 
-variable "zone" {}
+variable "zone" {
+  description = "Service zone"
+}
 
-variable "service_account_name" {}
+variable "service_account_name" {
+  description = "Service account."
+}
 
-variable "service_account_email" {}
+variable "service_account_email" {
+  description = "Service account email Id."
+}
 
-variable "service_name" {}
+variable "service_name" {
+  description = "gcp-cloud-run-service."
+}
 
 #----------------------------------------------
 # VM Instance variables
 #----------------------------------------------
-variable "instance_name" {}
+variable "instance_name" {
+  description = "VM instance name"
+}
 
-variable "machine_type" {}
+variable "machine_type" {
+  description = "vm machine type"
+}
 
-variable "compute_network" {}
+variable "compute_network" {
+  description = "compute network"
+}
 
 #----------------------------------------------
 # CI/CD Build variables
 #----------------------------------------------
-variable "repository_name" {}
+variable "repository_name" {
+  description = "terraform-module"
+}
 
-variable "branch_name" {}
+variable "branch_name" {
+  description = "branch name"
+}
 
-variable "image_name" {}
+variable "image_name" {
+  description = "image name."
+}
