ee8bec8c5ea2d5ac8d8c3318e07789fbf9be2d4e
Sumit <sumits@incubxperts.com>
2022-04-25 19:05:58 +0530

    Added terraform module

--- variables.tf
+++ variables.tf
@@ -1,31 +1,21 @@
 variable "service_account_credentials" {}
-
 variable "project_name" {}
-
 variable "region" {}
-
 variable "zone" {}
-
 variable "service_account_name" {}
-
 variable "service_account_email" {}
-
 variable "service_name" {}
 
 #----------------------------------------------
 # VM Instance variables
 #----------------------------------------------
 variable "instance_name" {}
-
 variable "machine_type" {}
-
 variable "compute_network" {}
 
 #----------------------------------------------
 # CI/CD Build variables
 #----------------------------------------------
 variable "repository_name" {}
-
 variable "branch_name" {}
-
 variable "image_name" {}
