2dd48673b969372d8923d20c5e0bfe2f4b150944
Sumit <sumits@incubxperts.com>
2022-04-26 12:28:04 +0530

    refactor: updated vm instance varibales

--- main.tf
+++ main.tf
@@ -1,17 +1,7 @@
-module "vm_instance_1" {
+module "ix-vm_instance" {
   source          = "./vm-instance-module"
   instance_name   = var.instance_name
   instance_image  = var.instance_image
   machine_type    = var.machine_type
   compute_network = var.compute_network
 }
-
-
-# module "connector_cicd" {
-#   source                = "./build-trigger-module"
-#   repository_name       = var.repository_name
-#   branch_name           = var.branch_name
-#   service_account_email = var.service_account_email
-#   service_name          = var.service_name
-#   region                = var.region
-# }
