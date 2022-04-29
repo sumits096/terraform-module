806c7e038c3ba795a978c65a0693147523b7e32d
yuvrajs-ix <yuvrajs@incubxperts.com>
2022-04-29 12:37:04 +0530

    change in email var

--- build-trigger-module/main.tf
+++ build-trigger-module/main.tf
@@ -114,13 +114,13 @@ resource "google_project_service" "iam" {
 #----------------------------------------------------------------------------------------------
 
 resource "google_project_iam_binding" "binding" {
-  members    = ["serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"]
+  members    = ["serviceAccount: _SERVICE_ACCOUNT_EMAIL"]
   role       = "roles/run.admin"
   depends_on = [google_project_service.build]
 }
 
 resource "google_project_iam_binding" "sa" {
-  members    = ["serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"]
+  members    = ["serviceAccount: _SERVICE_ACCOUNT_EMAIL"]
   role       = "roles/iam.serviceAccountUser"
   depends_on = [google_project_service.build]
 }
