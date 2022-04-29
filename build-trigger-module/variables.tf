6777b21e205a637d9bdb317be7d2ef9b769135db
yuvrajs-ix <yuvrajs@incubxperts.com>
2022-04-29 15:03:54 +0530

    vars changes

--- build-trigger-module/variables.tf
+++ build-trigger-module/variables.tf
@@ -1,7 +1,7 @@
 variable "project_name" {
   description = "The project ID where all resources will be launched."
   type        = string
-  default     = "terraform-deployment-demo"
+  default     = "studied-client-346013"
 }
 
 variable "region" {
@@ -19,13 +19,13 @@ variable "zone" {
 variable "service_account_email" {
   description = "Service account email Id."
   type        = string
-  default     = "terraform-deployment-demo-sa@terraform-deployment-demo.iam.gserviceaccount.com"
+  default     = "23130737730-compute@developer.gserviceaccount.com"
 }
 
 variable "service_name" {
   description = "The name of the Cloud Run service to deploy."
   type        = string
-  default     = "gcp-cloud-run-service"
+  default     = "23130737730-compute"
 }
 
 variable "repository_name" {
