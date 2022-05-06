3fb9afe03889cb4588166a090a50cc198668a012
Sumit <sumits@incubxperts.com>
2022-05-06 16:27:08 +0530

    Build trigger

--- main.tf
+++ main.tf
@@ -17,8 +17,7 @@ resource "google_cloudbuild_trigger" "terraform-trigger" {
     owner = "sumits096"
     name  = "https://github.com/sumits096/connector-gcp-test"
     push {
-      invert_regex = true
-      branch       = "main"
+      branch = "main"
     }
   }
 
