7c7b352d0c3a4332a0d5e4d0dfbc0ca13a95ff63
Sumit <sumits@incubxperts.com>
2022-05-06 15:21:29 +0530

    Build trigger

--- main.tf
+++ main.tf
@@ -6,36 +6,55 @@
 #   compute_network = var.compute_network
 # }
 
-resource "google_cloudbuild_trigger" "manual-trigger" {
-  name        = "Triggered"
-  description = "Triggered via terraform"
+resource "google_cloudbuild_trigger" "website-sanity-deploy-dev" {
+  name        = "sanity-webhook-trigger-dev"
+  description = "Trigger build on changes from sanity"
+  filename    = "/connector/workflow/cloudbuild.yml"
 
-  trigger_template {
-    branch_name = "main"
-    repo_name   = "https://github.com/sumits096/connector-gcp-test"
+  build_source = {
+    uri  = "https://github.com/sumits096/connector-gcp-test"
+    ref  = "main" # this should probably be branch, and instead add "/refs/heads/<branchname>" in code, no regex allowed.
+    type = "GITHUB"
   }
 
-  service_account = var.service_account_email
-  filename        = "connector/workflow/cloudbuild.yaml"
-
-  # source_to_build {
-  #   uri       = "https://hashicorp/terraform-provider-google-beta"
-  #   ref       = "refs/heads/main"
-  #   repo_type = "GITHUB"
-  # }
-
-  # git_file_source {
-  #   path      = "cloudbuild.yaml"
-  #   uri       = "https://hashicorp/terraform-provider-google-beta"
-  #   revision  = "refs/heads/main"
-  #   repo_type = "GITHUB"
-  # }
-
-  # approval_config {
-  #   approval_required = true
-  # }
-
+  substitutions = {
+    _DIR             = "website/"
+    _BUILD_ENV       = "development"
+    _NODE_VERSION    = "12-alpine3.12"
+    _SUPPRESS_OUTPUT = "true"
+  }
 }
+
+# resource "google_cloudbuild_trigger" "manual-trigger" {
+#   name        = "Triggered"
+#   description = "Triggered via terraform"
+
+#   trigger_template {
+#     branch_name = "main"
+#     repo_name   = "https://github.com/sumits096/connector-gcp-test"
+#   }
+
+#   service_account = var.service_account_email
+#   filename        = "connector/workflow/cloudbuild.yaml"
+
+#   source_to_build {
+#     uri       = "https://hashicorp/terraform-provider-google-beta"
+#     ref       = "refs/heads/main"
+#     repo_type = "GITHUB"
+#   }
+
+#   git_file_source {
+#     path      = "cloudbuild.yaml"
+#     uri       = "https://hashicorp/terraform-provider-google-beta"
+#     revision  = "refs/heads/main"
+#     repo_type = "GITHUB"
+#   }
+
+#   approval_config {
+#     approval_required = true
+#   }
+
+# }
 
 # resource "google_cloudbuild_trigger" "build_trigger" {
 #   trigger_template {
