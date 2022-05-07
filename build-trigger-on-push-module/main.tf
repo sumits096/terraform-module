0645093a2fe6b6f18575cd8df0fab323b60d9e84
Sumit <sumits@incubxperts.com>
2022-05-07 10:25:25 +0530

    updated cicd script

--- build-trigger-on-push-module/main.tf
+++ build-trigger-on-push-module/main.tf
@@ -1,3 +1,30 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = var.service_account_credentials
+  project     = var.project_name
+  region      = var.region
+  zone        = var.zone
+}
+
+provider "google-beta" {
+  credentials = var.service_account_credentials
+  project     = var.project_name
+  region      = var.region
+  zone        = var.zone
+}
+
 resource "google_cloudbuild_trigger" "terraform-trigger" {
   provider    = google-beta
   name        = var.build_trigger_name
